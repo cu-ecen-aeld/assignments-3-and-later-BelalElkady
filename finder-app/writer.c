4838e07128c5e3f2dd506cc55e329da58411b26b
belal <belal@ubuntu.myguest.virtualbox.org>
2024-05-23 19:03:30 +0200

    Fixed writer and Makefile

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -10,24 +10,25 @@ int main (int argc, char* argv[]){
     if(argc < 2 ){
         syslog(LOG_ERR, "Number of args is less than 2");
         closelog();
-        return 0 ; 
+        return 1 ; 
     }
     char* filePath = argv[1];
-    printf("file path :  %s",filePath);
+    printf("file path :  %s\n",filePath);
     char* strToBeAdded = argv[2];
+    printf("String :  %s\n",strToBeAdded);
     ssize_t nr;
     int fd = open(filePath,O_RDWR | O_CREAT |O_TRUNC,S_IRWXU|S_IRWXG|S_IRWXO);
     if(fd == -1) {
         syslog(LOG_ERR, "File cannot be opened");
         closelog();
-        return 0 ; 
+        return 1 ; 
     }
 
     nr = write(fd,strToBeAdded,strlen(strToBeAdded));
     if(nr == -1) {
         syslog(LOG_ERR, "Failed writing to the file");
         closelog();
-        return 0 ; 
+        return 1 ; 
     }else {
         syslog(LOG_DEBUG , "Writing %s to %s",strToBeAdded,filePath);
     }
